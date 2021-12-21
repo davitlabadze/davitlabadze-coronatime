a021be8ec25ea572dd70b061cbae2613426802f2
davitlabadze <labadzedato18@gmail.com>
2021-12-21 23:26:32 +0400

    fix(confirmation-email):design for mobile

--- resources/views/livewire/confirmation-email.blade.php
+++ resources/views/livewire/confirmation-email.blade.php
@@ -1,9 +1,11 @@
 <div>
-    <div class="grid mt-40 justify-items-center sm:ml-16">
-        <img src="{{ asset('/img/logo.svg') }}" alt="logo">
-            <div class="grid justify-items-center mt-252">
-                <img src="{{ asset('/img/checked.svg') }}" alt="logo">
-                <h1 class="mt-16 font-normal text-18 text-dark-100">We have sent you a confirmation email</h1>
-           </div>
+    <div class="grid mt-40">
+        <div class="grid justify-items-center sm:ml-16 sm:justify-start">
+            <img src="{{ asset('/img/logo.svg') }}" alt="logo" >
+        </div>
+        <div class="grid justify-items-center mt-252 sm:mt-214">
+            <img src="{{ asset('/img/checked.svg') }}" alt="logo">
+            <h1 class="mt-16 font-normal text-18 text-dark-100">We have sent you a confirmation email</h1>
+        </div>
     </div>
 </div>
