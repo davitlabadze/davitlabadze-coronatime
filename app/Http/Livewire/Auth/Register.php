84911e03317a12d2db4322182c406443248b5177
davitlabadze <labadzedato18@gmail.com>
2021-12-29 11:31:42 +0400

    wip:work on registration progress Do validation

--- app/Http/Livewire/Auth/Register.php
+++ app/Http/Livewire/Auth/Register.php
@@ -2,10 +2,41 @@
 
 namespace App\Http\Livewire\Auth;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Register extends Component
 {
+    public $name;
+    public $email;
+    public $password;
+    public $password_confirmation;
+
+    protected $rules = [
+        'email'    => 'required|email|unique:users,email',
+        'name'     => 'required|min:3|unique:users,name',
+        'password' => 'required|confirmed|min:3',
+        'password_confirmation' => 'required|min:3',
+    ];
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+
+    public function submit()
+    {
+        $this->validate();
+
+        User::create([
+            'email'    => $this->email,
+            'name'     => $this->name,
+            'password' => $this->password,
+        ]);
+
+        return redirect()->route('login');
+    }
     public function render()
     {
         return view('livewire.auth.register');
--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,33 +1,36 @@
 <div>
     <div class="flex ">
         <div class="w-full ml-108 sm:ml-16 ">
-            <form action="#">
+            <form action="#" wire:submit.prevent="submit">
                 <img src="{{ asset('/img/logo.svg') }}" alt="logo" class="mt-40 sm:mt-24 ">
                 <div class="mt-30 sm:mt-43">
                     <h1 class="font-black text-dark-100 text-xxl">Welcome back</h1>
                     <p class="mt-16 font-normal text-dark-60 sm:mt-8">Please enter required info to sign up</p>
                     <div class="mt-24">
                         <label for="username" class="block font-bold text-dark-100 text-xxs">Username</label>
-                        <input class="h-56 p-24 mt-8 border rounded w-392 border-inner-border sm:w-343" type="text" placeholder="Enter unique username or email" />
+                        <input id="name" class="h-56 p-24 mt-8 border rounded w-392 border-inner-border sm:w-343 outline-0 @error('name') border-2 border-system-error  @enderror"  type="name" wire:model="name" placeholder="Enter unique username" value="{{ old('name') }}" required autocomplete="name"  />
+                        @error('name') <span class="text-system-error">{{ $message }}</span> @enderror
+
+                    </div>
+                    <div class="mt-24">
+                        <label for="email" class="block font-bold text-dark-100 text-xxs">Email</label>
+                        <input id="email" class="h-56 p-24 mt-8 border rounded w-392 border-inner-border sm:w-343 outline-0 @error('email') border-2 border-system-error  @enderror" type="email" wire:model="email" placeholder="Enter your email" />
+                        @error('email') <span class="text-system-error"">{{ $message }}</span> @enderror
                     </div>
                     <div class="mt-24">
                         <label for="password" class="block font-bold text-dark-100 text-xxs">Password</label>
-                        <input class="h-56 p-24 mt-8 border rounded w-392 border-inner-border sm:w-343" type="password" placeholder="Fill in password" />
+                        <input id="password" class="h-56 p-24 mt-8 border rounded w-392 border-inner-border sm:w-343 outline-0 @error('password') border-2 border-system-error  @enderror" type="password" wire:model="password" placeholder="Fill in password" />
+                        @error('password') <span class="text-system-error"">{{ $message }}</span> @enderror
                     </div>
                     <div class="mt-24">
-                        <label for="password1" class="block font-bold text-dark-100 text-xxs">Repeat Password</label>
-                        <input class="h-56 p-24 mt-8 border rounded w-392 border-inner-border sm:w-343" type="password" placeholder="Repeat password" />
+                        <label for="confir-password" class="block font-bold text-dark-100 text-xxs">Repeat Password</label>
+                        <input id="password-confirmation" class="h-56 p-24 mt-8 border rounded w-392 border-inner-border sm:w-343 outline-0 @error('password_confirmation') border-2 border-system-error  @enderror" type="password" wire:model="password_confirmation" placeholder="Repeat password" />
+                        @error('password_confirmation') <span class="text-system-error"">{{ $message }}</span> @enderror
                     </div>
-                    <div class="flex mt-25">
-                       <div class="flex">
-                        <input type="checkbox" class="w-20 h-20 mt-1 border-inner-border sm:mt-0"/>
-                        <p class="ml-8 font-semibold text-dark-100 sm:text-xxs">Remember this device</p>
-                       </div>
 
-                    </div>
-                    <button type="submit" class="h-56 rounded-lg bg-brand-secondary w-392 mt-26 sm:w-343"><p class="font-black text-dark-fff">SIGN UP</p></button>
+                    <button type="submit" value="register" class="h-56 rounded-lg bg-brand-secondary w-392 mt-26 sm:w-343 outline-0"><p class="font-black text-dark-fff">SIGN UP</p></button>
                     <div class="flex items-center justify-center mt-24 -ml-20 w-96 sm:">
-                        <p class="font-normal text-dark-60">Don't have and account?</p>
+                        <p class="font-normal text-dark-60">Already have an account?</p>
                         <a href="{{ route('login') }}" class="font-bold text-dark-100">Log in</a>
                     </div>
                 </div>
