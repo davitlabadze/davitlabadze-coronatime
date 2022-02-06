c3be68e4f19c12d5c1b7c0ce943d561bc9037bf3
davitlabadze <labadzedato18@gmail.com>
2022-02-06 16:04:20 +0400

    style(logout): add modal

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -7,33 +7,19 @@
                     <x-change-lang />
                 </div>
                 <div class="lg:hidden">
-                    <div x-data="{isOpen:false}">
-                        <button @click="isOpen = !isOpen" type="button" class="block lg:hidden">
-                            <img class="w-6 h-6 mr-4" x-show="isOpen" src="{{ asset('img/close.svg') }}" alt="x" />
-                            <img x-show="!isOpen" class="mr-4" src="{{ asset('img/burgermenu.svg') }}"
-                                alt="burger menu logo">
-                            <div x-show="isOpen" @click.away="isOpen = false"
-                                class="absolute right-0 flex rounded-lg w-96 h-900 mt-7 bg-dark-4">
-                                <div class="z-50 w-full">
-                                    <div class="p-2 mt-2 border-b rounded border-b-dark-100 b">
-                                        <h1 class="font-bold text-16">{{ $user->name }}</h1>
-                                        {{-- <h1 class="font-bold text-16">Takeshi K.</h1> --}}
-                                    </div>
-                                    <div
-                                        class="h-auto p-2 rounded-lg ml-28 mt-52 w-44 bg-dark-60 hover:bg-brand-secondary hover:text-dark-fff">
-                                        <livewire:logout />
-                                    </div>
-                                </div>
-                            </div>
-                        </button>
-                    </div>
+                    <button x-data @click="$dispatch('custom-logout-modal')" type="button" class="block lg:hidden">
+                        <img   class="mr-4" src="{{ asset('img/burgermenu.svg') }}" alt="burger menu logo" />
+                    </button>
+                    <livewire:logout />
                 </div>
                 <div>
-                    {{-- <h1 class="font-bold text-16 sm:hidden">Takeshi K.</h1> --}}
                     <h1 class="font-bold text-16 sm:hidden">{{ $user->name }}</h1>
                 </div>
                 <div class="w-1 h-32 ml-16 mr-16 bg-dark-20 sm:hidden"></div>
                 <div class="sm:hidden">
+                    <a href="#" x-data x-cloak
+                        @click="$dispatch('custom-logout-modal')"
+                        class="font-medium cursor-pointer ">{{ __('Log Out') }}</a>
                     <livewire:logout />
                 </div>
             </div>
@@ -45,5 +31,4 @@
     <div>
         @yield('content')
     </div>
-
 </div>
--- resources/views/livewire/logout.blade.php
+++ resources/views/livewire/logout.blade.php
@@ -1,3 +1,37 @@
-<div>
-    <a class="font-medium cursor-pointer" wire:click="logout">{{ __("Log Out") }}</a>
+<div x-cloak>
+    <div
+    x-data="{ isOpen: false }"
+    x-show="isOpen"
+    @keydown.escape.window="isOpen = false"
+    @custom-logout-modal.window="isOpen = true"
+    class="fixed inset-0 z-10 overflow-y-auto"
+    aria-labelledby="modal-title"
+    role="dialog"
+    aria-modal="true">
+        <div class="flex items-center justify-center min-h-screen">
+            <div class="fixed inset-0 transition-opacity bg-opacity-75 bg-dark-60" aria-hidden="true"></div>
+            <div class="py-4 pt-5 pb-4 overflow-hidden transition-all transform rounded-lg bg-dark-fff sm:max-w-lg sm:w-full sm:p-6">
+                <div>
+                    <div class="px-3 mt-3 text-center sm:mt-5">
+                        <h3 class="text-lg font-medium leading-6 text-gray-900" id="modal-title">{{ __('Do you really want to log out?') }}</h3>
+                    </div>
+                </div>
+                <div class="flex mt-5 ml-3 mr-3 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
+                    <button
+                    type="button"
+                    @click="isOpen=false"
+                    class="inline-flex justify-center w-full px-4 py-2 text-base font-medium bg-white border rounded-md shadow-sm border-inner-border hover:bg-dark-60 hover:text-dark-fff sm:mt-0 sm:col-start-1 sm:text-sm">
+                    {{ __('Cancel') }}
+                </button>
+                    <button
+                        type="button"
+                        class="inline-flex justify-center w-full px-4 py-2 ml-3 text-base font-medium border border-transparent rounded-md shadow-sm text-dark-fff bg-brand-secondary sm:col-start-2 sm:text-sm"
+                        wire:click="logout">
+                        {{ __('Log Out') }}
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
