378a10e75b0c983283a0d1f8fb498faa23f30791
davitlabadze <labadzedato18@gmail.com>
2022-02-22 11:47:42 +0400

    fix(worldwide): change the logic of the sum

--- app/Http/Livewire/Worldwide.php
+++ app/Http/Livewire/Worldwide.php
@@ -9,24 +9,9 @@ class Worldwide extends Component
 {
     public function render()
     {
-        $confirmeds = Statistic::all()->pluck('confirmed');
-        $recovereds = Statistic::all()->pluck('recovered');
-        $deathss = Statistic::all()->pluck('deaths');
-
-        $confirmed = 0;
-        for ($i = 0; $i < count($confirmeds); $i++) {
-            $confirmed += $confirmeds[$i];
-        }
-
-        $recovered = 0;
-        for ($i = 0; $i < count($recovereds); $i++) {
-            $recovered += $recovereds[$i];
-        }
-
-        $deaths = 0;
-        for ($i = 0; $i < count($deathss); $i++) {
-            $deaths += $deathss[$i];
-        }
+        $confirmed = Statistic::sum('confirmed');
+        $recovered = Statistic::sum('recovered');
+        $deaths    = Statistic::sum('deaths');
         return view('livewire.worldwide', [
             'confirmed' => $confirmed,
             'recovered' => $recovered,
