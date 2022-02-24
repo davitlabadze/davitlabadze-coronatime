1df28258df712a6eb660ad9763538b99a63b3fc3
davitlabadze <labadzedato18@gmail.com>
2022-02-24 17:38:56 +0400

    fix(countries): remove comment

--- app/Http/Livewire/Countries.php
+++ app/Http/Livewire/Countries.php
@@ -7,41 +7,6 @@ use Livewire\Component;
 
 class Countries extends Component
 {
-    // public $search;
-    // public $sortBy = '';
-    // public $sortDirection = 'asc';
-
-    // protected $queryString = [
-    //     'sortBy'
-    // ];
-
-    // public function sortBy($column)
-    // {
-    //     $this->sortDirection = $this->sortBy === $column ? !$this->sortDirection : 'asc';
-    //     $this->sortBy = $column;
-    // }
-
-    // public function render()
-    // {
-    //     // $user   = auth()->user();
-    //     $locale = app()->currentLocale();
-    //     return view('livewire.countries', [
-    //         // 'user' => $user,
-    //         'countries' => Country::join('statistics', 'countries.id', '=', 'statistics.country_id')
-    //         ->orderBy($this->sortBy, $this->sortDirection ? 'asc' : 'desc')
-    //         ->when($this->sortDirection === false, function ($query) {
-    //             return $query->orderByDesc('name');
-    //         })
-    //         ->when($this->sortDirection === true, function ($query) {
-    //             return $query->orderBy('name', 'asc');
-    //         })
-    //         ->when(strlen($this->search) >= 2, function ($query) use ($locale) {
-    //             return $query->where("name->${locale}", 'like', '%'.$this->search.'%');
-    //         })
-    //         ->get()
-    //     ]);
-    // }
-
     public $search;
     public $sortBy = 'name';
     public $sortDirectionAsc = true;
